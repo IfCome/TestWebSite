c9dddd586434c3f6d42fb7bcfed9e5475c33f264
XunLoveXia <yang_xun@outlook.com>
2016-02-29 21:24:17 +0800

    创建项目

---
+++ AmazeUITest/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmazeUITest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult IndexPage()
+        {
+            return View();
+        }
+
+    }
+}
