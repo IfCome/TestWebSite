b14cf1856fe3b4dbcb954c62e66d5805de360109
yang_xun <yang_xun@outlook.com>
2016-03-01 18:32:34 +0800

    完成添加用户页面

---
+++ AmazeUITest/Controllers/UserInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmazeUITest.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        //
+        // GET: /UserInfo/
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+    }
+}
