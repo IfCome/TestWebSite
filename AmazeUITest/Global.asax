2e124e7f3322abf643960198c4085d2f5b4f0d79
yang_xun <yang_xun@outlook.com>
2016-03-08 15:23:37 +0800

    修改项目和解决方案名称

--- AmazeUITest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AmazeUITest.MvcApplication" Language="C#" %>
--- CrowdFundingShop.UI/Properties/AssemblyInfo.cs
+++ CrowdFundingShop.UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("AmazeUITest")]
+[assembly: AssemblyTitle("CrowdFundingShop.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AmazeUITest")]
+[assembly: AssemblyProduct("CrowdFundingShop.UI")]
 [assembly: AssemblyCopyright("版权所有(C)  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
