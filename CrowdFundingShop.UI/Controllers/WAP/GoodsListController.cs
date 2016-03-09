e09eb000b1a1f9ef8a0f36a67f15c8a4970cecc1
张三三 <zhangjianchang33@outlook.com>
2016-03-10 00:04:38 +0800

    新增Amaze UI(Test)

---
+++ CrowdFundingShop.UI/Controllers/WAP/GoodsListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrowdFundingShop.UI.Controllers.WAP
+{
+    public class GoodsListController : Controller
+    {
+        //
+        // GET: /GoodsList/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
